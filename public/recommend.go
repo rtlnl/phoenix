04e8e20623f48eb09469b02afde8586a97d58151
Davide Berdin <davideberdin@gmail.com>
2019-08-02 16:46:45 +0200

    added more endpoints

--- public/recommend.go
+++ public/recommend.go
@@ -4,10 +4,44 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rtlnl/data-personalization-api/pkg/db"
 	"github.com/rtlnl/data-personalization-api/utils"
 )
 
+// RecommendRequest is the object that represents the payload of the request for the recommend endpoint
+type RecommendRequest struct {
+	Signals          []Signal
+	PublicationPoint string
+	Campaign         string
+}
+
+// RecommendResponse is the object that represents the payload of the response for the recommend endpoint
+type RecommendResponse struct {
+	Signals         []Signal
+	Recommendations []string
+}
+
+// Signal is an alias that represents the signal defintion
+type Signal map[string]string
+
 // Recommend will take care of fetching the personalized content for a specific user
 func Recommend(c *gin.Context) {
-	utils.Response(c, http.StatusOK, "recommended")
+	_ = c.MustGet("AerospikeClient").(*db.AerospikeClient)
+
+	var rr RecommendRequest
+	if err := c.BindJSON(&rr); err != nil {
+		utils.ResponseError(c, http.StatusBadRequest, err)
+		return
+	}
+
+	// retrieve how the signal_key is composed
+	// Example:
+	//   - key: namespace#publicationPoint#campaign
+	//	   value: articleId_userId
+
+	// compose signal_key based on the value retrieved before
+
+	// get recommended items
+
+	utils.Response(c, http.StatusCreated, "recommended items")
 }
---
+++ public/version_test.go
@@ -0,0 +1,26 @@
+package public
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLongVersion(t *testing.T) {
+	router.GET("/", LongVersion)
+
+	code, body, err := MockRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.FailNow()
+	}
+
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		t.FailNow()
+	}
+
+	assert.Equal(t, http.StatusOK, code)
+	assert.Equal(t, "{\"version\":\"Public Personalization APIs v0.0.1\"}", string(b))
+}
