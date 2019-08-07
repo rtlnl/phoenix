1857b7f053c1b236356523965766ab84061582e4
Davide Berdin <davideberdin@gmail.com>
2019-08-07 22:37:03 +0200

    new docker compose file

--- models/model_test.go
+++ models/model_test.go
@@ -1,21 +1,24 @@
 package models
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/rtlnl/data-personalization-api/pkg/db"
+	"github.com/rtlnl/data-personalization-api/utils"
 )
 
-const (
-	testDBHost    = "127.0.0.1"
-	testDBPort    = 3000
+var (
+	testDBHost    = utils.GetEnv("DB_HOST", "127.0.0.1")
+	testDBPort    = utils.GetEnv("DB_PORT", "3000")
 	testNamespace = "test"
 )
 
 func TestNewModel(t *testing.T) {
-	ac := db.NewAerospikeClient(testDBHost, testNamespace, testDBPort)
+	p, _ := strconv.Atoi(testDBPort)
+	ac := db.NewAerospikeClient(testDBHost, testNamespace, p)
 
 	// Test object creation
 	m, err := NewModel("rtl_test", "homepage", "userID_articleID", ac)
